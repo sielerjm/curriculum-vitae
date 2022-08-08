8b2a94625b4dbcc267e975a75474ce83529d6b4a
Michael Sieler <sielerjm@oregonstate.edu>
2022-08-08 11:33:40 -0700

    update render script

--- render_PDF.R
+++ render_PDF.R
@@ -1,22 +1,50 @@
 
 # HTML to PDF -------------------------------------------------------------
 
-outType = "CV" # "Resume" or "CV"
+output.dir = "/Users/michaelsieler/Dropbox/Mac (2)/Documents/Project_Repos/michaelsieler/ResumeCV/"
+user.name = "MichaelSieler"
 
-pagedown::chrome_print(
-    rmarkdown::render("curriculum-vitae.Rmd",
-                      output_file = paste0("MichaelSieler_", outType),
-                      params = list(
-                          outType = outType  # "Resume" or "CV"
-                      ))
-    )
+
+## Resume ------------------------------------------------------------------
 
 outType = "Resume" # "Resume" or "CV"
 
 pagedown::chrome_print(
     rmarkdown::render("curriculum-vitae.Rmd",
-                      output_file = paste0("MichaelSieler_", outType),
+                      output_file = paste0(user.name, "_", outType),
                       params = list(
                           outType = outType  # "Resume" or "CV"
                       ))
 )
+
+
+## CV ----------------------------------------------------------------------
+
+outType = "CV" # "Resume" or "CV"
+
+pagedown::chrome_print(
+    rmarkdown::render("curriculum-vitae.Rmd",
+                      output_file = paste0(user.name, "_", outType),
+                      params = list(
+                          outType = outType  # "Resume" or "CV"
+                      ))
+)
+
+## Copy Files to Other Directory -------------------------------------------
+
+### Resume
+file.copy(from = paste0(getwd(), "/", user.name, "_Resume.pdf"),
+          to = paste0(output.dir, user.name, "_Resume.pdf"),
+          overwrite = T
+          ) 
+
+### CV
+file.copy(from = paste0(getwd(), "/", user.name, "_CV.pdf"),
+          to = paste0(output.dir, user.name, "_CV.pdf"),
+          overwrite = T
+          )
+
+
+# Completed! --------------------------------------------------------------
+
+print("Completed HTML to PDF")
